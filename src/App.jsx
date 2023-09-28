d01c88010a459ad7e7c45976f0b916407caf21bd
Dev-Simon20 <jeanpierks6@gmail.com>
2023-09-28 18:37:40 -0500

    Iniciando el proyecto twiter

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import TwiterCard from './assets/componentes/TwiterCard';
+
+
+function App() {
+  return (
+    <>
+    <TwiterCard  nombre={'Jeampierk Simon Contreras'} correo={'jeanpierks6@gmail.com'}/>
+    <TwiterCard  nombre={'Frank Simon Contreras'} correo={'frankSimon@gmail.com'}/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/componentes/TwiterCard/index.jsx
@@ -0,0 +1,22 @@
+
+const TwiterCard=(props)=>{
+  const {nombre,correo}=props
+    return <article className="tw-followCard">
+    <header  className="tw-followCard-header">
+      <img
+       className="tw-followCard-avatar"
+        src="https://avatars.githubusercontent.com/u/70038546?v=4"
+        alt="El avatar de SimonDev"
+      />
+      <div className='tw-followCard-info'>
+        <strong>{nombre}</strong>
+        <span className='tw-followCard-infoUser' >{correo}</span>
+      </div>
+    </header>
+    <aside className="tw-followCard-aside">
+      <button className='tw-followCard-button'>seguir</button>
+    </aside>
+  </article>
+}
+
+export default TwiterCard;
